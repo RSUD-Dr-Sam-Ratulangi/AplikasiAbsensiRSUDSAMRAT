3d88cfa40eda4352164eaf5ca19fa09b5a8b5970
po3rdlp <leonpolandos53@gmail.com>
2023-12-30 14:01:28 +0800

    update

---
+++ RNAbsensiRSUDSAMRAT/src/components/Modal/index.tsx
@@ -0,0 +1,208 @@
+import React, {useRef, useEffect, useState} from 'react';
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  Text,
+  Pressable,
+  View,
+  Animated,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
+
+interface ModalAppProps {
+  modalVisible: boolean;
+  setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedDateData: any; // Adjust the type accordingly
+}
+
+const ModalApp: React.FC<ModalAppProps> = ({
+  modalVisible,
+  setModalVisible,
+  selectedDateData,
+}) => {
+  const [checkInPressed, setCheckInPressed] = useState(false);
+  const [checkOutPressed, setCheckOutPressed] = useState(false);
+
+  const handleCheckInPress = () => {
+    // Handle Check In click
+    console.log('Check In Clicked');
+    setCheckInPressed(true);
+    setCheckOutPressed(false);
+  };
+
+  const handleCheckOutPress = () => {
+    // Handle Check Out click
+    console.log('Check Out Clicked');
+    setCheckInPressed(false);
+    setCheckOutPressed(true);
+  };
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const clockInTime = (selectedDateData?.attendances[0]?.clockIn || '').replace(
+    'T',
+    '  ',
+  );
+  const clockOutTime = selectedDateData?.attendances[0]?.clockOut
+    ? selectedDateData.attendances[0].clockOut.replace('T', ' ')
+    : 'Belum Absen Pulang';
+  const selfieCheckIn = selectedDateData?.attendances[0]?.selfieCheckIn;
+  const selfieCheckOut = selectedDateData?.attendances[0]?.selfieCheckOut;
+  const imageSourceCheckIn = {uri: `data:image/jpeg;base64,${selfieCheckIn}`};
+  const imageSourceCheckOut = {uri: `data:image/jpeg;base64,${selfieCheckOut}`};
+
+  useEffect(() => {
+    if (modalVisible) {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [fadeAnim, modalVisible]);
+
+  const closeModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  return (
+    <Animated.View style={[styles.centeredView, {opacity: fadeAnim}]}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <Pressable style={styles.overlay} onPress={closeModal}>
+          <Animated.View style={styles.modalContainer}>
+            <View style={styles.modalView}>
+              <View style={styles.checkInOrOut}>
+                <TouchableHighlight
+                  onPress={handleCheckInPress}
+                  underlayColor="transparent"
+                  style={[
+                    styles.checkInButton,
+                    checkInPressed ? styles.pressedButton : null,
+                  ]}>
+                  <Text>Check In</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                  onPress={handleCheckOutPress}
+                  underlayColor="transparent"
+                  style={[
+                    styles.checkOutButton,
+                    checkOutPressed ? styles.pressedButton : null,
+                  ]}>
+                  <Text>Check Out</Text>
+                </TouchableHighlight>
+              </View>
+              <View>
+                {checkInPressed && clockInTime && (
+                  <Text style={styles.modalText}>{clockInTime}</Text>
+                )}
+                {checkOutPressed && clockOutTime && (
+                  <Text style={styles.modalText}>{clockOutTime}</Text>
+                )}
+              </View>
+              {checkInPressed && imageSourceCheckIn && (
+                <Image style={styles.modalImage} source={imageSourceCheckIn} />
+              )}
+
+              {checkOutPressed && imageSourceCheckOut && (
+                <Image style={styles.modalImage} source={imageSourceCheckOut} />
+              )}
+            </View>
+          </Animated.View>
+        </Pressable>
+      </Modal>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    opacity: 1, // Initial opacity
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalImage: {
+    width: 300,
+    height: 350,
+    marginBottom: 10,
+    borderRadius: 13,
+    resizeMode: 'stretch',
+  },
+  checkInOrOut: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginTop: 15,
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  gap: {
+    marginRight: 10, // Adjust the value to set the desired gap
+  },
+  checkInButton: {
+    marginRight: 10,
+    padding: 10,
+    borderColor: 'green',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  checkOutButton: {
+    padding: 10,
+    borderColor: 'red',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  pressedButton: {
+    backgroundColor: 'rgba(0, 128, 0, 0.1)', // Change this color as needed
+  },
+});
+
+export default ModalApp;
--- RNAbsensiRSUDSAMRAT/src/pages/History/index.tsx
+++ RNAbsensiRSUDSAMRAT/src/pages/History/index.tsx
@@ -1,232 +1,275 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, Dimensions, ActivityIndicator, ScrollView, RefreshControl } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { Calendar } from 'react-native-calendars'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Dimensions,
+  ActivityIndicator,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Calendar} from 'react-native-calendars';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ModalApp from '../../components/Modal';
 
 const History = ({navigation}: any) => {
-    const screenWidht = Dimensions.get('window').width;
+  const screenWidht = Dimensions.get('window').width;
 
-    const [status, setStatus] = useState('');
-    const [location, setLocation] = useState('');
-    const [timeIn, setTimeIn] = useState('');
-    const [timeOut, setTimeOut] = useState('');
-    const [dateCheckIn, setDateCheckIn] = useState('');
-    const [dateCheckOut, setDateCheckOut] = useState('');
-    const [data, setData] = useState([]);
-    const [getMarkedDates, setGetMarkedDates] = useState();
-    const [selectedDateData, setSelectedDateData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isCheckOut, setIsCheckOut] = useState(false);
-    const [isEnableDesc, setIsEnableDesc] = useState(false);
-    const getdate = String(new Date().getDate()).padStart(2, '0'); 
-    const getmonth = String(new Date().getMonth() + 1).padStart(2, '0'); 
-    const getyear = String(new Date().getFullYear()).padStart(2, '0');
-    const currentDate = getyear + '-' + getmonth + '-' + getdate;
-    const [refreshing, setRefreshing] = useState(false);
+  const [status, setStatus] = useState('');
+  const [location, setLocation] = useState('');
+  const [timeIn, setTimeIn] = useState('');
+  const [timeOut, setTimeOut] = useState('');
+  const [dateCheckIn, setDateCheckIn] = useState('');
+  const [dateCheckOut, setDateCheckOut] = useState('');
+  const [data, setData] = useState([]);
+  const [getMarkedDates, setGetMarkedDates] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isCheckOut, setIsCheckOut] = useState(false);
+  const [isEnableDesc, setIsEnableDesc] = useState(false);
+  const getdate = String(new Date().getDate()).padStart(2, '0');
+  const getmonth = String(new Date().getMonth() + 1).padStart(2, '0');
+  const getyear = String(new Date().getFullYear()).padStart(2, '0');
+  const currentDate = getyear + '-' + getmonth + '-' + getdate;
+  const [refreshing, setRefreshing] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedDateData, setSelectedDateData] = useState<any | null>(null);
 
-    const handleRefresh = () => {
-        setRefreshing(true);
-        setIsLoading(true);
+  //   const handleDayPress = () => {
+  //     setModalVisible(true);
+  //   };
 
-        getEmployeeId()
-        .then(() => {
-            setRefreshing(false);
-        })
-        .catch(() => {
-            setRefreshing(false);
-        });
+  const handleRefresh = async () => {
+    setRefreshing(true);
+
+    try {
+      await getEmployeeId();
+    } catch (error) {
+      console.error('Error handling refresh:', error.message);
+    } finally {
+      setRefreshing(false);
     }
-    
-    const getEmployeeId = async () => {
-        const employeeId = await AsyncStorage.getItem('employeeId');
-        getData(employeeId);
+  };
+
+  const getEmployeeId = async () => {
+    const employeeId = await AsyncStorage.getItem('employeeId');
+    getData(employeeId);
+  };
+
+  const getData = async employeeId => {
+    setIsLoading(true);
+    const url = `http://rsudsamrat.site:9999/api/v1/dev/attendances/filter?employeeId=${employeeId}`;
+
+    try {
+      const response = await axios.get(url);
+      console.log('Data:', response.data);
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    } finally {
+      setIsLoading(false);
     }
-    
-    const getData = (employeeId) => {
-        setIsLoading(true);
-        const url = `http://rsudsamrat.site:9999/api/v1/dev/attendances/filter?employeeId=${employeeId}`;
-        axios.get(url)
-        .then(function (response) {
-            setData(response.data);
-            setIsLoading(false)
-        })
-        .catch(function (error) {
-            console.log(error);
-            setIsLoading(false);
-        });
+  };
+
+  useEffect(() => {
+    getEmployeeId();
+  }, []);
+
+  const handleDayPress = day => {
+    const selectedData = data.find(schedule =>
+      schedule.attendances?.some(
+        attendance => attendance.scheduleDate === day.dateString,
+      ),
+    );
+
+    if (selectedData) {
+      setModalVisible(true);
+      console.log('ini selected data', selectedData);
+      const clockInTime = selectedData.attendances[0].clockIn.slice(11, 19);
+      const clockOutTime = selectedData.attendances[0].clockOut;
+      const formattedClockOutTime =
+        clockOutTime !== null ? clockOutTime.slice(11, 19) : null;
+      const formattedDateCheckOut =
+        clockOutTime !== null ? clockOutTime.slice(0, 10) : null;
+
+      setSelectedDateData(selectedData);
+      setIsEnableDesc(true);
+      setIsCheckOut(clockOutTime !== null);
+      setTimeOut(
+        formattedClockOutTime ? `Check-Out -> ${formattedClockOutTime}` : '',
+      );
+      setDateCheckOut(formattedDateCheckOut || '');
+      setIsCheckOut(Boolean(formattedClockOutTime));
+      setTimeIn(`Check-In    -> ${clockInTime}`);
+      setDateCheckIn(selectedData.attendances[0].scheduleDate);
+      setStatus(`Check-In (${selectedData.attendances[0].attendanceType})`);
+
+      if (selectedData.attendances[0].location === null) {
+        setLocation(
+          'RSUD DR SAM RATULANGI TONDANO, Kembuan, Tondano Utara, Minahasa, Sulawesi Utara',
+        );
+      } else {
+        setLocation(selectedData.attendances[0].location);
+      }
+    } else {
+      setTimeIn('');
+      setTimeOut('');
+      setDateCheckIn('-');
+      setStatus(`Tidak ada riwayat pekerjaan pada ${day.dateString}`);
+      setIsEnableDesc(false);
+      setLocation('-');
     }
+  };
 
-    useEffect(() => {
-        getEmployeeId();
-    }, [])
-
-    useEffect(() => {
-        const markedDates = data.reduce((result, schedule) => {
-            schedule.attendances.forEach(attendance => {
-                const { scheduleDate, attendanceState } = attendance;
-                if (scheduleDate) {
-                    result[scheduleDate] = { selected: true, selectedColor:  getColorForAttendanceState(attendanceState)};
-                }
-            });
-            return result;
-        }, {});
-
-        setGetMarkedDates(markedDates);
-    }, [data]);
-
-    const getColorForAttendanceState = (attendanceState) => {
-        switch (attendanceState) {
-            case "ONTIME":
-                return "#14F42B";
-            case "ALPHA":
-                return "#F41414";
-            case "LATE":
-                return "#F0F414";
-            default:
-                return "#F41414";
+  useEffect(() => {
+    const markedDates = data.reduce((result, schedule) => {
+      schedule.attendances.forEach(attendance => {
+        const {scheduleDate, attendanceState} = attendance;
+        if (scheduleDate) {
+          result[scheduleDate] = {
+            selected: true,
+            selectedColor: getColorForAttendanceState(attendanceState),
+          };
         }
+      });
+      return result;
+    }, {});
+
+    setGetMarkedDates(markedDates);
+  }, [data]);
+
+  const getColorForAttendanceState = attendanceState => {
+    switch (attendanceState) {
+      case 'ONTIME':
+        return '#14F42B';
+      case 'ALPHA':
+        return '#F41414';
+      case 'LATE':
+        return '#3DF414';
+      default:
+        return '#F41414';
     }
+  };
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={handleRefresh}
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+        }>
+        {isLoading ? (
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size={'large'} />
+          </View>
+        ) : (
+          <>
+            <Image
+              source={require('./../../assets/images/Ilustration4.png')}
+              style={{width: screenWidht, height: 50}}
+              resizeMode="cover"
+            />
+            <Calendar
+              style={{
+                height: 350,
+              }}
+              current={currentDate}
+              onDayPress={handleDayPress}
+              markedDates={getMarkedDates}
+            />
+            <View>
+              <Image
+                source={require('./../../assets/images/Ilustration5.png')}
+                style={{width: screenWidht, height: 110, position: 'absolute'}}
+                resizeMode="cover"
+              />
+              <Text style={styles.status}>{status}</Text>
+              {isEnableDesc ? (
+                <View style={styles.desc}>
+                  <View style={styles.locationContainer}>
+                    <Image
+                      source={require('./../../assets/icons/IconLocation.png')}
+                      style={{width: 38, height: 38}}
                     />
-                }
-            >
-                    {isLoading? (
-                        <ActivityIndicator
-                            size={'large'}
-                            style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
-                        />
-                    ) : (
-                    <>
-                    <Image 
-                        source={require('./../../assets/images/Ilustration4.png')}
-                        style={{width: screenWidht, height: 50}}
-                        resizeMode='cover'
-                        />
-                    <Calendar
-                    style={{
-                        height: 350,
-                    }}
-                    current={currentDate}
-                    onDayPress={day => {
-                        const selectedData = data.find(schedule => schedule.attendances?.some(attendance => attendance.scheduleDate === day.dateString));
-                        if (selectedData) {
-                            setSelectedDateData(selectedData);
-                            setIsEnableDesc(true);
-                            const clockInTimeString = selectedData.attendances[0].clockIn;
-                            const clockOutTimeString = selectedData.attendances[0].clockOut;
-                            const formattedClockInTime = clockInTimeString.slice(11, 19);
-                            
-                            if(clockOutTimeString !== null){
-                                const formattedClockOutTime = clockOutTimeString.slice(11, 19);
-                                const formattedDateCheckOut = clockOutTimeString.slice(0, 10);
-                                setTimeOut('Check-Out -> '+ formattedClockOutTime);
-                                setDateCheckOut(formattedDateCheckOut);
-                                setIsCheckOut(true);
-                            } else {
-                                setIsCheckOut(false);
-                            }
-
-                            setTimeIn('Check-In    -> '+ formattedClockInTime);
-                            setDateCheckIn(selectedData.attendances[0].scheduleDate);
-                            setStatus('Check-In ('+selectedData.attendances[0].attendanceType+')');
-                            if(selectedData.attendances[0].location === null){
-                                setLocation('RSUD DR SAM RATULANGI TONDANO, Kembuan, Tondano Utara, Minahasa, Sulawesi Utara');
-                            } else {
-                                setLocation(selectedData.attendances[0].location);
-                            }
-                        } else {
-                            setTimeIn('');
-                            setTimeOut('');
-                            setDateCheckIn('-');
-                            setStatus('Tidak ada riwayat pekerjaan pada '+day.dateString);
-                            setIsEnableDesc(false);
-                            setLocation('-');
-                        }
-                    }}
-                    markedDates={getMarkedDates}
+                    <Text style={styles.text}>{location}</Text>
+                  </View>
+                  <View style={styles.timeContainer}>
+                    <Image
+                      source={require('./../../assets/icons/IconTime.png')}
+                      style={{width: 33, height: 33, marginRight: 2}}
                     />
-                <View>
-                    <Image 
-                        source={require('./../../assets/images/Ilustration5.png')}
-                        style={{width: screenWidht, height: 110, position: 'absolute'}}
-                        resizeMode='cover'
-                        />
-                    <Text style={styles.status}>{status}</Text>
-                    { isEnableDesc ? (
-                        <View style={styles.desc}>
-                            <View style={styles.locationContainer}>
-                                <Image 
-                                    source={require('./../../assets/icons/IconLocation.png')}
-                                    style={{width: 38, height: 38}}
-                                    />
-                                <Text style={styles.text}>{location}</Text>
-                            </View>
-                            <View style={styles.timeContainer}>
-                                <Image 
-                                    source={require('./../../assets/icons/IconTime.png')}
-                                    style={{width: 33, height: 33, marginRight: 2}}
-                                    />
-                                <Text style={styles.text}> {timeIn} {dateCheckIn}</Text>
-                            </View>
-                            { isCheckOut ? (
-                                <View style={styles.timeContainer}>
-                                    <Image 
-                                        source={require('./../../assets/icons/IconTime.png')}
-                                        style={{width: 33, height: 33, marginRight: 2}}
-                                        />
-                                    <Text style={styles.text}> {timeOut} {dateCheckOut}</Text>
-                                </View>
-                            ) : (
-                                <></>
-                                )}
-                        </View>
-                    ) : (<></>)}
+                    <Text style={styles.text}>
+                      {' '}
+                      {timeIn} {dateCheckIn}
+                    </Text>
+                  </View>
+                  {isCheckOut ? (
+                    <View style={styles.timeContainer}>
+                      <Image
+                        source={require('./../../assets/icons/IconTime.png')}
+                        style={{width: 33, height: 33, marginRight: 2}}
+                      />
+                      <Text style={styles.text}>
+                        {' '}
+                        {timeOut} {dateCheckOut}
+                      </Text>
+                    </View>
+                  ) : (
+                    <></>
+                  )}
                 </View>
-                </>
-                )}
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+              ) : (
+                <></>
+              )}
+              {/* Modal component */}
+              <ModalApp
+                modalVisible={modalVisible}
+                setModalVisible={setModalVisible}
+                selectedDateData={selectedDateData} // Pass the data as a prop
+              />
+            </View>
+          </>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default History
+export default History;
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#ffffff',
-    },
-    status: {
-        fontSize: 25,
-        color: '#292F2F',
-        paddingHorizontal: 27
-    },
-    desc:{
-        marginLeft: 15,
-        marginTop: 20,
-        marginBottom: 90
-    },
-    text:{
-        fontSize: 17,
-        color: '#666666',
-        width: '80%',
-        marginLeft: 7
-    },
-    locationContainer:{
-        flexDirection: 'row',
-    },
-    timeContainer:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginLeft: 3,
-        marginTop: 15,
-    },
-})
+  container: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+  },
+  status: {
+    fontSize: 25,
+    color: '#292F2F',
+    paddingHorizontal: 27,
+  },
+  desc: {
+    marginLeft: 15,
+    marginTop: 20,
+    marginBottom: 90,
+  },
+  text: {
+    fontSize: 17,
+    color: '#666666',
+    width: '80%',
+    marginLeft: 7,
+  },
+  locationContainer: {
+    flexDirection: 'row',
+  },
+  timeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 3,
+    marginTop: 15,
+  },
+  loadingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
